6a5894941b3b04311434bcc03fae51aa2ce2bb15
Yaaoqb <83239269+Yaaoqb@users.noreply.github.com>
2023-05-19 01:15:24 +0800

    Delete SpringBoot_CRUD_Example directory

--- SpringBoot_CRUD_Example/src/main/java/com/example/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-    // Find a product by name
-    Product findByName(String name);
-}
